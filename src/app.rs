93318a7d3d0ffded4d1683abccc76e77903814a1
Abhay Sood <abhay@measure.sh>
2026-03-20 11:02:33 +0530

    Add panel focus state and key handling

--- src/app.rs
+++ src/app.rs
@@ -7,12 +7,14 @@ pub enum Action {
 
 pub struct App {
     visible: [bool; 7],
+    focused: Option<u8>,
 }
 
 impl App {
     pub fn new() -> Self {
         Self {
             visible: [true; 7],
+            focused: None,
         }
     }
 
@@ -23,8 +25,23 @@ impl App {
                 self.toggle_panel(c as u8 - b'0');
                 Action::None
             }
+            KeyCode::Char('i') => { self.focus_panel(1); Action::None }
+            KeyCode::Char('l') => { self.focus_panel(2); Action::None }
+            KeyCode::Char('c') => { self.focus_panel(7); Action::None }
             _ => Action::None,
         }
+    }
+
+    fn focus_panel(&mut self, n: u8) {
+        if self.focused == Some(n) {
+            self.focused = None;
+        } else {
+            self.focused = Some(n);
+        }
+    }
+
+    pub fn focused_panel(&self) -> Option<u8> {
+        self.focused
     }
 
     fn toggle_panel(&mut self, n: u8) {
@@ -113,5 +130,38 @@ mod tests {
         let mut app = App::new();
         assert!(matches!(app.handle_key(KeyCode::Char('x')), Action::None));
         assert_eq!(app.panel_visibility(), &[true; 7]);
+    }
+
+    #[test]
+    fn focus_toggles_on_same_key() {
+        let mut app = App::new();
+        app.handle_key(KeyCode::Char('i'));
+        assert_eq!(app.focused_panel(), Some(1));
+        app.handle_key(KeyCode::Char('i'));
+        assert_eq!(app.focused_panel(), None);
+    }
+
+    #[test]
+    fn focus_switches_between_panels() {
+        let mut app = App::new();
+        app.handle_key(KeyCode::Char('i'));
+        assert_eq!(app.focused_panel(), Some(1));
+        app.handle_key(KeyCode::Char('l'));
+        assert_eq!(app.focused_panel(), Some(2));
+        app.handle_key(KeyCode::Char('c'));
+        assert_eq!(app.focused_panel(), Some(7));
+    }
+
+    #[test]
+    fn unfocusable_keys_do_not_focus() {
+        let mut app = App::new();
+        app.handle_key(KeyCode::Char('n'));
+        assert_eq!(app.focused_panel(), None);
+    }
+
+    #[test]
+    fn new_app_has_no_focus() {
+        let app = App::new();
+        assert_eq!(app.focused_panel(), None);
     }
 }
