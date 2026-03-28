02c069422f58c055a8d5d4a2e2f9294aff7082e2
Abhay Sood <abhay@measure.sh>
2026-03-28 15:33:11 +0530

    Use arboard for native cross-platform clipboard

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,25 +1,5 @@
-use std::io::Write;
-use std::process::{Command, Stdio};
-
 pub fn copy_to_clipboard(text: &str) {
-    let (cmd, args): (&str, &[&str]) = if cfg!(target_os = "macos") {
-        ("pbcopy", &[])
-    } else if cfg!(target_os = "linux") {
-        ("xclip", &["-selection", "clipboard"])
-    } else if cfg!(target_os = "windows") {
-        ("clip", &[])
-    } else {
-        return;
-    };
-
-    if let Ok(mut child) = Command::new(cmd)
-        .args(args)
-        .stdin(Stdio::piped())
-        .spawn()
-    {
-        if let Some(stdin) = child.stdin.as_mut() {
-            let _ = stdin.write_all(text.as_bytes());
-        }
-        let _ = child.wait();
+    if let Ok(mut clipboard) = arboard::Clipboard::new() {
+        let _ = clipboard.set_text(text);
     }
 }
